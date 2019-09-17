3425cf4d1a30bd2a59a76b7ba76912f725d34f86
aeddi <antoine.e.b@gmail.com>
2019-09-17 16:23:31 +0200

    refactor: init a clean react-native project

--- example/react-native/android/app/src/main/java/com/ipfsmobiledemo/MainActivity.java
+++ example/react-native/android/app/src/main/java/com/ipfsmobiledemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ipfs;
+package com.ipfsmobiledemo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ipfs";
+        return "IPFSMobileDemo";
     }
 }
--- example/react-native/ios/ipfs/bridge/IpfsBridge.m
+++
@@ -1,8 +0,0 @@
-//
-//  IpfsBridge.m
-//  ipfs
-//
-//  Created by Guilhem Fanton on 16/09/2019.
-//
-
-#import <Foundation/Foundation.h>
